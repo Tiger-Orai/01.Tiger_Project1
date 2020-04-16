2b09db9363b4b866a33fff7e50e20969e9e3ca38
Yinhu Wang <Tiger@Tigers-MacPro.local>
2020-04-16 17:25:30 -0400

    first commit

---
+++ TigerCalculator.py
@@ -0,0 +1,17 @@
+#Project Name: Calculator for 2 numbers
+#Enter numbers
+num1 = float(input("Enter number 1___"))
+num2 = float(input("Enter number 2___"))
+# Enter operator
+operator = input("Enter operator")
+
+if operator == "+":
+    print(num1," ",operator," ",num2," =", num1+num2)
+elif operator == "-":
+    print(num1," ",operator," ",num2," =", num1-num2)
+elif operator == "*":
+    print(num1," ",operator," ",num2," =", num1*num2)
+elif operator == "/":
+    print(num1," ",operator," ",num2," =", num1/num2)
+else:
+    print("Operator not recognized")
